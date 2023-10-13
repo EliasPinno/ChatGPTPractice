0214e87f5edc5c6aa4637a252884fd79c6af9f71
EliasPinno <pucas946@gmail.com>
2023-10-13 14:21:48 -0600

    Altered main.py to not use command line rn.

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ import DocumentCleaningUtil
 import sys
 
 if __name__ == '__main__':
-    DocumentCleaningUtil.clean_document(sys.argv[1])
+    #DocumentCleaningUtil.clean_document(sys.argv[1])
+    DocumentCleaningUtil.clean_document("inputText.txt")
