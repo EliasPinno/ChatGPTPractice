e0ae38cae0d93a0d3a78af718b3ba397640deac8
EliasPinno <pucas946@gmail.com>
2023-10-13 14:22:29 -0600

    Added a chatGPT request properly formatted.

--- DocumentCleaningUtil.py
+++ DocumentCleaningUtil.py
@@ -1,6 +1,6 @@
 import openai
 import os
-
+import json
 
 def clean_document(document_path: str):
     file_content_string = None
@@ -10,5 +10,14 @@ def clean_document(document_path: str):
 
 
 def clean_text(text_to_clean: str):
-    openai.api_key = os.getenv("OPENAI_API_KEY")
-    pass
+    openai.api_key = os.environ.get("OPENAI_API_KEY")
+    model = "gpt-3.5-turbo"
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": "Give a basic response to whatever you are prompted with."},
+            {"role": "user", "content": text_to_clean}
+        ],
+    )
+    print(response)
+    return response
